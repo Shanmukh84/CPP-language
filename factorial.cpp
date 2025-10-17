0eba1fca21d76b3c1545815d5344e987164464d5
Shanmukh84 <24B11DS025@adityauniversity.in>
2025-10-17 12:04:01 +0530

    Add files via upload

--- factorial.cpp
+++ factorial.cpp
@@ -1,22 +1,3 @@
-//write a program to check number is prime or not.
-#include<stdio.h>
-int main()
-{
-	int n,i,count=0;
-	printf("enter the number:");
-	scanf("%d",&n);
-	for(i=1;i<=n;i++)
-	{
-		if(n%i==0)
-		count=count+1;
-	}
-	if(count==2)
-	{
-		printf("prime number");
-	}
-	else
-	{
-		printf("not a prime number");
-	}
-	return 0;
-}
+#include <iostream>
+using namespace std;
+int main(){int n;cin>>n;long long f=1;for(int i=1;i<=n;i++)f*=i;cout<<f;return 0;}
